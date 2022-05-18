12c2c6c4d20c17af7a91b05fe88d21c7323ea3a4
DanielleLarces <danielle.santos@larces.uece.br>
2022-05-18 08:35:51 -0300

    Modified __init__.py file

--- Flask API/app/__init__.py
+++ Flask API/app/__init__.py
@@ -4,10 +4,10 @@ from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Admin538*@localhost:3306/flask_jwt_users'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../databases/flask_jwt_users.db'
 app.config['SQLALCHEMY_BINDS'] = {
-    'db2': 'mysql://root:Admin538*@localhost:3306/flask_jwt_appmeta',
-    'db3': 'mysql://root:Admin538*@localhost:3306/flask_jwt_maindb'
+    'db2': 'sqlite:///../databases/flask_jwt_appmeta.db',
+    'db3': 'sqlite:///../databases/flask_jwt_maindb.db'
 }
 app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
