b79250f23dabc37e6e24315d09e1677921d6c18c
DanielleLarces <danielle.santos@larces.uece.br>
2022-07-06 12:50:38 -0300

    Modified SSE files to remove csv

--- SSE/build_index.py
+++ SSE/build_index.py
@@ -2,8 +2,9 @@ import pandas as pd
 from Crypto.Cipher import AES
 from Crypto.Hash import MD5
 from Crypto.Random import random
+#import time
 import numpy as np
-import time
+import sqlite3
 
 
 def build_trapdoor(MK, keyword):
@@ -20,52 +21,85 @@ def build_codeword(ID, trapdoor):
     return ECB_cipher.encrypt(ID_index.digest()).hex()
 
 
-def build_index(MK, ID, keyword_list):
-    secure_index = [0] * len(keyword_list)
-    for i in range(len(keyword_list)):
-        codeword = build_codeword(ID, build_trapdoor(MK, keyword_list[i]))
+def build_index(MK, ID, record_columns_list):
+    secure_index = [0] * len(record_columns_list)
+    for i in range(len(record_columns_list)):
+        codeword = build_codeword(ID, build_trapdoor(MK, record_columns_list[i]))
         secure_index[i] = codeword
     random.shuffle(secure_index)
     return secure_index
 
-def searchable_encryption(raw_data_file_name, master_key, keyword_type_list):
-    raw_data = pd.read_csv(raw_data_file_name)
-    features = list(raw_data)
-    raw_data = raw_data.values
-
-    keyword_number = [i for i in range(0, len(features)) if features[i] in keyword_type_list]
-
+def searchable_encryption(master_key, columns_list, table_name, cursor, connection_db):
+    #start_time = time.time()
     index_header = []
-    for i in range(1, len(keyword_type_list) + 1):
+    for i in range(1, len(columns_list) + 1):
         index_header.append("index_" + str(i))
 
     document_index = []
-    start_time = time.time()
+    query = "SELECT * from " + table_name
+    cursor.execute(query)
+
+    results = cursor.fetchall()
+
+    #print(results)
+
+    from_db = []
+    for result in results:
+        result = list(result)
+        from_db.append(result)
+
+    #print(from_db)
+
+    raw_data = pd.DataFrame(from_db, columns=columns_list)
+    features = list(raw_data)
+    raw_data = raw_data.values
+
+    column_number = [i for i in range(0, len(features)) if features[i] in columns_list]
+
     for row in range(raw_data.shape[0]):
         record = raw_data[row]
-        record_keyword_list = [record[i] for i in keyword_number]
-        record_index = build_index(master_key, row, record_keyword_list)
+        record_columns_list = [record[i] for i in column_number]
+        record_index = build_index(master_key, row, record_columns_list)
         document_index.append(record_index)
 
-    time_cost = time.time() - start_time
-    print(time_cost)
     document_index_dataframe = pd.DataFrame(np.array(document_index), columns=index_header)
-    document_index_dataframe.to_csv(raw_data_file_name.split(".")[0] + "_index.csv")
+    new_file_name = table_name + "_index"
+    document_index_dataframe.to_sql(new_file_name, connection_db, if_exists='replace', index=False)
 
+    #time_cost = time.time() - start_time
+    #print(time_cost)
 
 if __name__ == "__main__":
 
-    document_name = "Database.csv" #input("Please input the file to be encrypted:  ")
+    document_name = "Database.db" #name of the database to be encrypted
+    connection = sqlite3.connect(document_name)
+    cursor = connection.cursor()
 
-    master_key_file_name = "masterkey" #input("Please input the file stored the master key:  ")
+    table_name = "users" #name of the table in database to be encrypted
+
+    master_key_file_name = "masterkey" #password autentication
     master_key = open(master_key_file_name).read()
     if len(master_key) > 16:
         print("the length of master key is larger than 16 bytes, only the first 16 bytes are used")
         master_key = bytes(master_key[:16])
 
-    keyword_list_file_name = "keywordlist" #input("please input the file stores keyword type:  ")
-    keyword_type_list = open(keyword_list_file_name).read().split(",")
+    #keyword_list_file_name = "keywordlist" #name of the columns in database
+    #keyword_type_list = open(keyword_list_file_name).read().split(",")
 
-    searchable_encryption(document_name, master_key, keyword_type_list)
+    columns_list = []
+    data = cursor.execute("SELECT * from " + table_name)
+    for column in data.description:
+        columns_list.append(column[0])
+
+    connection_db = sqlite3.connect('Encrypted_Database')
+    cursor_db = connection_db.cursor()
+
+    #print(columns_list)
+
+    searchable_encryption(master_key, columns_list, table_name, cursor, connection_db)
+
+    connection_db.commit()
+    cursor.close()
+    cursor_db.close()
 
     print("Finished")
--- SSE/trapdoor_and_search.py
+++ SSE/trapdoor_and_search.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from Crypto.Cipher import AES
 from Crypto.Hash import MD5
+import sqlite3
 
 
 def build_trapdoor(MK, keyword):
@@ -15,11 +16,23 @@ def build_codeword(ID, trapdoor):
     ECB_cipher = AES.new(trapdoor, AES.MODE_ECB)
     return ECB_cipher.encrypt(ID_index.digest()).hex()
 
-def search_index(document, trapdoor):
+def search_index(index_table_name, trapdoor, cursor, columns_list):
     search_result = []
-    data_index = pd.read_csv(document)
-    data_index = data_index.values
+    #data_index = pd.read_csv(document)
+    #data_index = data_index.values
     # start_time = time.time()
+    query = "SELECT * from " + index_table_name
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    from_db = []
+    for result in results:
+        result = list(result)
+        from_db.append(result)
+
+    data_index = pd.DataFrame(from_db, columns=columns_list)
+    data_index = data_index.values
+
     for row in range(data_index.shape[0]):
         if build_codeword(row, trapdoor) in data_index[row]:
             search_result.append(row)
@@ -37,16 +50,27 @@ if __name__ == "__main__":
         print("the length of master key is larger than 16 bytes, only the first 16 bytes are used")
         master_key = bytes(master_key[:16])
 
-
     trapdoor_file = open(keyword + "_trapdoor", "wb")
     trapdoor_of_keyword = build_trapdoor(master_key, keyword)
     trapdoor_file.write(trapdoor_of_keyword)
     trapdoor_file.close()
 
-    index_file_name = "Database_index.csv" #input("Please input the index file you want to search:  ")
+    database_name = "Encrypted_Database"
+    index_table_name = "users_index" #input("Please input the index file you want to search:  ")
+    connection = sqlite3.connect(database_name)
+    cursor = connection.cursor()
+
+    columns_list = []
+    data = cursor.execute("SELECT * from " + index_table_name)
+    for column in data.description:
+        columns_list.append(column[0])
+
     keyword_trapdoor = keyword + "_trapdoor" #input("Please input the file stored the trapdoor you want to search:  ")
     #keyword_trapdoor = open(keyword_trapdoor).read().strip()
     with open(keyword_trapdoor, "rb") as f:
         keyword_trapdoor = f.read()
-    search_result = search_index(index_file_name, keyword_trapdoor)
+    search_result = search_index(index_table_name, keyword_trapdoor, cursor, columns_list)
+
+    cursor.close()
+    
     print("The identifiers of files that contain the keyword are: \n", search_result)
