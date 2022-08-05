bc26abce239a0c196cf176d808322cc1dad6a8aa
Danielle Larces <danielle.santos@aluno.uece.br>
2022-08-05 16:11:19 -0300

    Add modified version of search_hash_row.py and trapdoor_and_search.py with SQLAlchemy and modified requirements.txt

--- SSE/search_hash_row.py
+++ SSE/search_hash_row.py
@@ -1,20 +1,17 @@
-import sqlite3
+from sqlalchemy import create_engine
 
-def search_hash_row (tn, cursor, hash):
-    query = "SELECT * from " + tn + " WHERE line_hash = \"%s\"" % (hash)
-    cursor.execute(query)
-    
-    results = cursor.fetchall()
-    print(results)
+def search_hash_row (tn, engine, hash):
+    with engine.connect() as connection:
+        query = "SELECT * from " + tn + " WHERE line_hash = \"%s\"" % (hash)
+        result = connection.execute(query)
+        for row in result:
+            print(row)
 
 if __name__ == "__main__":
     table_name = input("Please input the table you want to search into: ")
     hash_value = input("Input the desired row hash value: ")
 
     database_name = "NewDatabase" 
-    connection = sqlite3.connect(database_name)
-    cursor = connection.cursor()
+    engine = create_engine(f"sqlite:///{database_name}")
 
-    search_hash_row(table_name, cursor, hash_value)
-
-    cursor.close()
+    search_hash_row(table_name, engine, hash_value)
--- SSE/trapdoor_and_search.py
+++ SSE/trapdoor_and_search.py
@@ -1,8 +1,9 @@
+import time
 import pandas as pd
+import sqlite3
 from Crypto.Cipher import AES
 from Crypto.Hash import MD5
-import sqlite3
-import time
+from sqlalchemy import create_engine
 
 
 def build_trapdoor(MK, keyword):
@@ -17,36 +18,37 @@ def build_codeword(ID, trapdoor):
     ECB_cipher = AES.new(trapdoor, AES.MODE_ECB)
     return ECB_cipher.encrypt(ID_index.digest()).hex()
 
-def search_index(index_table_name, trapdoor, cursor, columns_list):
+def search_index(index_table_name, trapdoor, engine, columns_list):
     start_time = time.time()
     search_result = []
     #data_index = pd.read_csv(document)
     #data_index = data_index.values
     # start_time = time.time()
-    query = "SELECT * from " + index_table_name
-    cursor.execute(query)
+    with engine.connect() as connection:
+        query = "SELECT * from " + index_table_name
+        result_proxy = connection.execute(query)
 
-    size = 1000
-    results = cursor.fetchmany(size)
-    #results = cursor.fetchall()
+        size = 1000
+        results = result_proxy.fetchmany(size)
+        #results = cursor.fetchall()
 
-    from_db = []
-    while results:
-        for result in results:
-            result = list(result)
-            from_db.append(result)
-        
-        results = cursor.fetchmany(size)
+        from_db = []
+        while results:
+            for result in results:
+                result = list(result)
+                from_db.append(result)
+            
+            results = result_proxy.fetchmany(size)
 
-    data_index = pd.DataFrame(from_db, columns=columns_list)
-    data_index = data_index.values
+        data_index = pd.DataFrame(from_db, columns=columns_list)
+        data_index = data_index.values
 
-    for row in range(data_index.shape[0]):
-        if build_codeword(row, trapdoor) in data_index[row]:
-            search_result.append(row+1)
+        for row in range(data_index.shape[0]):
+            if build_codeword(row, trapdoor) in data_index[row]:
+                search_result.append(row+1)
 
-    #time_cost = time.time() - start_time
-    #print(time_cost)
+        #time_cost = time.time() - start_time
+        #print(time_cost)
     
     return search_result
 
@@ -69,24 +71,23 @@ if __name__ == "__main__":
 
     database_name = "EncryptedDB"
     index_table_name = table_name + "_index" #input("Please input the index file you want to search:  ")
-    connection = sqlite3.connect(database_name)
-    cursor = connection.cursor()
+
+    engine = create_engine(f"sqlite:///{database_name}")
 
     columns_list = []
-    data = cursor.execute("SELECT * from " + index_table_name)
-    for column in data.description:
-        columns_list.append(column[0])
+    data = engine.execute("SELECT * FROM " + index_table_name)
+    data_description = list(data.keys())
+    for column in data_description:
+        columns_list.append(column)
 
     keyword_trapdoor = keyword + "_trapdoor" #input("Please input the file stored the trapdoor you want to search:  ")
     #keyword_trapdoor = open(keyword_trapdoor).read().strip()
     with open(keyword_trapdoor, "rb") as f:
         keyword_trapdoor = f.read()
-    search_result = search_index(index_table_name, keyword_trapdoor, cursor, columns_list)
+    search_result = search_index(index_table_name, keyword_trapdoor, engine, columns_list)
 
     or_database_name = "NewDatabase"
     or_connection = sqlite3.connect(or_database_name)
     or_cursor = or_connection.cursor()
-    
-    cursor.close()
     
     print("The identifiers of files that contain the keyword are: \n", search_result)
